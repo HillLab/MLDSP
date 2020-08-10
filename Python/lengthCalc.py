64d795b3ab3c62d29724625f08ed933dd57d445f
Wanxin Li <wanxinli@gra-login1.graham.sharcnet>
2020-08-10 00:04:43 -0400

    add lengthCalc.py

---
+++ Python/lengthCalc.py
@@ -0,0 +1,13 @@
+from statistics import median, mean
+
+def lengthCalc(seqList):
+    """calculates length stats
+
+    Keyword arguments:
+    seqList: a list of squence
+    """
+    lenList = map(len, seqList)
+    maxLen = max(lenList)
+    minLen = min(lenList)
+    meanLen = mean(lenList)
+    medLen = median()
