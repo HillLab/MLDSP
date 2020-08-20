4a6e49d752476190a9f2d795f990ae1281be160d
Wanxin Li <wanxinli@gra-login2.graham.sharcnet>
2020-08-20 12:22:08 -0400

    added cgr.py

---
+++ Python/cgr.py
@@ -0,0 +1,26 @@
+import numpy as np 
+
+def cgr(chars, order, k):
+    """computes CGR representation
+
+    Keyword arguments:
+    chars: sequence
+    order: chars to include in CGR
+    k: value of k-mer
+    """
+    out = np.zeros(2**k)
+    x = 2**(k-1)
+    y = 2**(k-1)
+
+    for i in len(chars):
+        char = chars[i]
+        x = np.fix(x/2)
+        if char == order[2] or char == order[3]:
+            x += 2**(k-1)
+        y = np.fix(y/2)
+        if char == order[0] or char == order[3]:
+            y += 2**(k-1)
+
+        if (i+1) >= k:
+            out[y+1, x+1] += 1
+    return out 
