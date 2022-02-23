7553e16e9e545b5fd04368e55033ddf85f9f7c78
jshleap <jshleap@gmail.com>
2022-02-23 11:29:42 -0500

    Added the option to preprocess the query data which does not have metadata, and some of the typings got adjusted

--- MLDSP_core/preprocessing.py
+++ MLDSP_core/preprocessing.py
@@ -1,6 +1,6 @@
 from collections import Counter
 from pathlib import Path
-from typing import Dict
+from typing import Dict, Optional, Tuple
 
 from pyfaidx import Fasta
 
@@ -22,7 +22,9 @@ def csv2dict(infile: Path) -> Dict[str, str]:
     return dictionary
 
 
-def preprocessing(data_set: Path, metadata: Path):
+def preprocessing(data_set: Path, metadata: Optional[Path],
+                  prefix: str = 'Train') -> Tuple[
+    Fasta, int, Optional[Dict[str, str]], Optional[Counter]]:
     """
     TODO: update these doc strings
     Preprocessing of fasta sequences using BioPython into a database of
@@ -43,9 +45,12 @@ def preprocessing(data_set: Path, metadata: Path):
 
     """
     # Iterate through all fasta files and read it into data structure
-    outfn = data_set.joinpath('all_seqs.fasta').resolve()
-    cluster_dict = csv2dict(metadata)
-    cluster_stats = Counter(cluster_dict.values())
+    outfn = data_set.joinpath(f'{prefix}_all_seqs.fasta').resolve()
+    if metadata is not None:
+        cluster_dict = csv2dict(metadata)
+        cluster_stats = Counter(cluster_dict.values())
+    else:
+        cluster_dict = cluster_stats = None
     if outfn.exists():
         print(f'File {outfn} exists and will be used! If this is '
               f'unintended, please remove the file')
@@ -55,10 +60,11 @@ def preprocessing(data_set: Path, metadata: Path):
                 with open(file) as infile, open(outfn, 'a') as outfile:
                     outfile.write(f'{infile.read().strip()}\n')
     seq_dict = Fasta(str(outfn))
-    difference = set(cluster_dict.keys()).difference(seq_dict.keys())
-    if difference:
-        raise Exception(f"Your metadata and a your fasta don't match,"
-                        f" check your input\nCulprit(s) "
-                        f"{''.join(difference)}")
+    if metadata is not None:
+        difference = set(cluster_dict.keys()).difference(seq_dict.keys())
+        if difference:
+            raise Exception(f"Your metadata and a your fasta don't match,"
+                            f" check your input\nCulprit(s) "
+                            f"{''.join(difference)}")
     total_seq = len(seq_dict.keys())
     return seq_dict, total_seq, cluster_dict, cluster_stats
